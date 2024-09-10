4497e826161e1da852436ce3c04206b059f55ed0
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-10 17:22:21 +0300

    Authenticated app using kinde-auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardIndexPage() {
+    return (
+        <div className="">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const {getUser} = getKindeServerSession()
+  const session = await getUser();
   return (
-   <div className="">
-    <h1>Hello world</h1>
-    <Button>Click</Button>
-   </div>
+    <div className="p-10">
+      <h1>Hello world</h1>
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ): (
+        <div className="">
+          <RegisterLink>
+        <Button>Register</Button>
+      </RegisterLink>
+      <LoginLink>
+        <Button>Login</Button>
+      </LoginLink>
+        </div>
+      )}
+    </div>
   );
 }
