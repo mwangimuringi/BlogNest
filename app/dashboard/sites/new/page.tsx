9003e5ada3b7ac2c2c6afa402bc54cd7eb7fb805
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-11 01:17:11 +0300

    validation of form using zod&conform

--- app/dashboard/sites/new/page.tsx
+++ app/dashboard/sites/new/page.tsx
@@ -1,12 +1,40 @@
-import { Label } from "@radix-ui/react-dropdown-menu";
+"use client";
 
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { CreateSiteAction } from "@/app/actions";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { siteSchema } from "@/app/utils/zodSchemas";
+import { useFormState } from "react-dom";
+// import { SubmitButton } from "@/app/components/dashboard/SubmitButtons";
 
 export default function NewSiteRoute() {
-    return(
-        <div className="flex flex-col flex-1 items-center justify-center">
+  const [lastResult, action] = useFormState(CreateSiteAction, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: siteSchema,
+      });
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
       <Card className="max-w-[450px]">
         <CardHeader>
           <CardTitle>Create Site</CardTitle>
@@ -14,52 +42,52 @@ export default function NewSiteRoute() {
             Create your Site here. Click the button below once your done...
           </CardDescription>
         </CardHeader>
-        {/* <form id={form.id} onSubmit={form.onSubmit} action={action}> */}
+        <form id={form.id} onSubmit={form.onSubmit} action={action}>
           <CardContent>
             <div className="flex flex-col gap-y-6">
               <div className="grid gap-2">
                 <Label>Site Name</Label>
-                {/* <Input
+                <Input
                   name={fields.name.name}
                   key={fields.name.key}
                   defaultValue={fields.name.initialValue}
                   placeholder="Site Name"
                 />
-                <p className="text-red-500 text-sm">{fields.name.errors}</p> */}
+                <p className="text-red-500 text-sm ">{fields.name.errors}</p>
               </div>
 
               <div className="grid gap-2">
                 <Label>Subdirectory</Label>
-                {/* <Input
+                <Input
                   name={fields.subdirectory.name}
                   key={fields.subdirectory.key}
                   defaultValue={fields.subdirectory.initialValue}
                   placeholder="Subdirectory"
-                /> */}
-                {/* <p className="text-red-500 text-sm">
+                />
+                <p className="text-red-500 text-sm">
                   {fields.subdirectory.errors}
-                </p> */}
+                </p>
               </div>
 
               <div className="grid gap-2">
                 <Label>Description</Label>
-                {/* <Textarea
+                <Textarea
                   name={fields.description.name}
                   key={fields.description.key}
                   defaultValue={fields.description.initialValue}
                   placeholder="Small Description for your site"
-                /> */}
-                {/* <p className="text-red-500 text-sm">
+                />
+                <p className="text-red-500 text-sm">
                   {fields.description.errors}
-                </p> */}
+                </p>
               </div>
             </div>
           </CardContent>
           <CardFooter>
             {/* <SubmitButton text="Create Site" /> */}
           </CardFooter>
-        {/* </form> */}
+        </form>
       </Card>
     </div>
-    )
+  );
 }
