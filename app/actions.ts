c44b2d53fec3692d0dc1e525076659f278ce23e3
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-12 15:23:23 +0300

    Added the edit article component

--- app/actions.ts
+++ app/actions.ts
@@ -56,3 +56,29 @@ export async function CreatePostAction(prevState: any, formData: FormData) {
 
   return redirect(`/dashboard/sites`);
 }
+
+export async function EditPostAction(prevState: any, formData: FormData) {
+  const user = await requireUser();
+  //compare form data with zod
+  const submission = parseWithZod(formData, {
+    schema: PostSchema,
+  });
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.post.update({
+    where: {
+      userId: user.id,
+      id: formData.get("articleId") as string,
+    },
+    data: {
+      title: submission.value.title,
+      smallDescription: submission.value.smallDescription,
+      slug: submission.value.slug,
+      articleContent: JSON.parse(submission.value.articleContent),
+      image: submission.value.coverImage,
+    },
+  });
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
