1339b6feaac65117ad4db67a5c018a4189221e0b
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-17 00:31:26 +0300

    Fixed linting error

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -2,38 +2,36 @@ import prisma from "@/app/utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  
-  console.log('User:', user); // Log user details
+export async function GET(request: Request) {
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    console.error('Error: User authentication failed');
-    return NextResponse.json({ error: "User authentication failed" }, { status: 401 });
-  }
+    if (!user || !user.id) {
+      return NextResponse.json({ error: "User authentication failed" }, { status: 401 });
+    }
 
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-  });
-
-  console.log('Database User:', dbUser); // Log user from DB
-
-  if (!dbUser) {
-    dbUser = await prisma.user.create({
-      data: {
+    let dbUser = await prisma.user.findUnique({
+      where: {
         id: user.id,
-        firstName: user.given_name ?? "",
-        lastName: user.family_name ?? "",
-        email: user.email ?? "",
-        profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
 
-    console.log('Created User:', dbUser); // Log created user
-  }
+    if (!dbUser) {
+      dbUser = await prisma.user.create({
+        data: {
+          id: user.id,
+          firstName: user.given_name ?? "",
+          lastName: user.family_name ?? "",
+          email: user.email ?? "",
+          profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+        },
+      });
+    }
 
-  return NextResponse.redirect("http://localhost:3000/dashboard/");
+    return NextResponse.redirect(new URL('/dashboard/', request.url));
+  } catch (error) {
+    console.error('Error in API route:', error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
