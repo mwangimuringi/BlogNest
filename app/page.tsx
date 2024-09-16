3732f57675d368daa9a6b4fa2e3fed36181361b9
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-16 18:29:46 +0300

    Rendered the logo components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Hero } from "./components/frontend/Hero";
+import { Logos } from "./components/frontend/Logos";
 
 export default async function Home() {
   const {getUser} = getKindeServerSession()
@@ -13,6 +14,7 @@ export default async function Home() {
   return (
    <div className="max-w-7xl mx-auto px-4 sm:px-8 mb-24">
       <Hero />
+      <Logos />
    </div>
   );
 }
