e8fc8cf81d6d543ec82fe8754d613c660d6aa0fc
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-16 23:28:46 +0300

    ...

--- app/blog/[name]/[slug]/page.tsx
+++ app/blog/[name]/[slug]/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { JSONContent } from "novel";
 
-export async function getData(slug: string) {
+async function getData(slug: string) {
   const data = await prisma.post.findUnique({
     where: {
       slug: slug,
@@ -28,15 +28,13 @@ export async function getData(slug: string) {
   return data;
 }
 
-export default async function BlogPostPage({
+export default async function SlugRoute({
   params,
 }: {
-  params: {
-    slug: string;
-    name: string;
-  };
+  params: { slug: string; name: string };
 }) {
   const data = await getData(params.slug);
+
   return (
     <>
       <div className="flex items-center gap-x-3 pt-10 pb-5">
@@ -45,36 +43,37 @@ export default async function BlogPostPage({
             <ArrowLeft className="size-4" />
           </Link>
         </Button>
-        <h1 className="text-xl font-semibold tracking-tight">Go Back</h1>
+        <h1 className="text-xl font-medium">Go Back</h1>
       </div>
 
       <div className="flex flex-col items-center justify-center mb-10">
         <div className="m-auto w-full text-center md:w-7/12">
           <p className="m-auto my-5 w-10/12 text-sm font-light text-muted-foreground md:text-base">
-            {new Intl.DateTimeFormat('en-US', {
-                dateStyle: 'medium',
-            }).format(new Date(data.createdAt))}
+            {new Intl.DateTimeFormat("en-US", {
+              dateStyle: "medium",
+            }).format(data.createdAt)}
           </p>
-          <h1 className="text-3xl font-bold tracking-tight md:text-5xl">
+          <h1 className="mb-5 text-3xl font-bold md:text-6xl tracking-tight">
             {data.title}
           </h1>
-          <p className="w-10/12 text-muted-foreground line-clamp-3 m-auto">
+          <p className="m-auto w-10/12 text-muted-foreground line-clamp-3">
             {data.smallDescription}
           </p>
         </div>
       </div>
-      <div className="relative mb-10 m-auto h-80 w-full max-w-screen-lg overflow-hidden md:mb-20 md:h-[450px] md:rounded-2xl lg:w-2/3">
+
+      <div className="relative m-auto mb-10 h-80 w-full max-w-screen-lg overflow-hidden md:mb-20 md:h-[450px] md:w-5/6 md:rounded-2xl lg:w-2/3">
         <Image
           src={data.image}
           alt={data.title}
-          className="object-cover h-full w-full"
           width={1200}
           height={630}
-          priority 
+          className="h-full w-full object-cover"
+          priority
         />
       </div>
 
-      <RenderArticle json={data.articleContent as JSONContent} />  
+      <RenderArticle json={data.articleContent as JSONContent} />
     </>
   );
 }
