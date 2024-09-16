8333c53cf0a5ee655118d85f5a5660517a797818
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-16 23:50:07 +0300

    ...

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -5,9 +5,12 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  
+  console.log('User:', user); // Log user details
 
   if (!user || user === null || !user.id) {
-    throw new Error("Something went wrong");
+    console.error('Error: User authentication failed');
+    return NextResponse.json({ error: "User authentication failed" }, { status: 401 });
   }
 
   let dbUser = await prisma.user.findUnique({
@@ -16,6 +19,8 @@ export async function GET() {
     },
   });
 
+  console.log('Database User:', dbUser); // Log user from DB
+
   if (!dbUser) {
     dbUser = await prisma.user.create({
       data: {
@@ -23,10 +28,11 @@ export async function GET() {
         firstName: user.given_name ?? "",
         lastName: user.family_name ?? "",
         email: user.email ?? "",
-        profileImage:
-          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+        profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
+
+    console.log('Created User:', dbUser); // Log created user
   }
 
   return NextResponse.redirect("http://localhost:3000/dashboard/");
