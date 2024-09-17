3ed85dee76b3693ddae5bfb4c1677fc9a834ea82
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-17 12:08:20 +0300

    Redeployment

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -2,36 +2,36 @@ import prisma from "@/app/utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  try {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || !user.id) {
-      return NextResponse.json({ error: "User authentication failed" }, { status: 401 });
-    }
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong");
+  }
 
-    let dbUser = await prisma.user.findUnique({
-      where: {
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
         id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
-
-    if (!dbUser) {
-      dbUser = await prisma.user.create({
-        data: {
-          id: user.id,
-          firstName: user.given_name ?? "",
-          lastName: user.family_name ?? "",
-          email: user.email ?? "",
-          profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-        },
-      });
-    }
-
-    return NextResponse.redirect(new URL('/dashboard/', request.url));
-  } catch (error) {
-    console.error('Error in API route:', error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
+
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "production"
+      ? "https://blog-nest-eight.vercel.app/dasboard"
+      : "http://localhost:3000/dashboard"
+  );
 }
