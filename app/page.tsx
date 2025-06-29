7dc138e6f48dea41af8f4050390f2b178ecc04b6
mwangimuringi <mwangimuringi10@gmail.com>
2025-06-29 13:50:51 +0300

    Completed styling of hero section

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,6 @@ export default async function Home() {
   return (
    <div className="max-w-7xl mx-auto px-4 sm:px-8 mb-24">
       <Hero />
-      <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"></div>
-      <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-         <div className="flex flex-col items-center justify-center gap-y-4 text-center"></div>
-      </div>
       <Logos />
       <Features />
       <PricingTable />
