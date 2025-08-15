74b56669ea214efc7f711dc92250978321ae0a73
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-15 17:45:40 +0300

    Completed the landing page

--- app/actions.ts
+++ app/actions.ts
@@ -122,6 +122,11 @@ export async function CreatePostAction(prevState: any, formData: FormData) {
 
 export async function EditPostActions(prevState: any, formData: FormData) {
   const user = await requireUser();
+  const site = await prisma.site.findUnique({
+    where: {
+      id: formData.get("siteId") as string,
+    },
+  });
   //compare form data with zod schema
   const submission = parseWithZod(formData, {
     schema: PostSchema,
