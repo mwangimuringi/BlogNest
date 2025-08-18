5f9b983ae020a327b48acf79babb2c5d6dfade76
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-18 14:37:29 +0300

    ...

--- app/dashboard/pricing/page.tsx
+++ app/dashboard/pricing/page.tsx
@@ -57,6 +57,8 @@ export default async function PricingPage() {
             Click on the button below, this will give you the opportunity to
             change your payment details and view your statement at the same
             time.
+            <br />
+            <br />
           </CardDescription>
         </CardHeader>
         <CardContent>
