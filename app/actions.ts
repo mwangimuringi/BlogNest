353a7430d4350ec11a47298a3e73aca9bf731039
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-18 14:33:11 +0300

    ...

--- app/actions.ts
+++ app/actions.ts
@@ -122,7 +122,6 @@ export async function CreatePostAction(prevState: any, formData: FormData) {
 
 export async function EditPostActions(prevState: any, formData: FormData) {
   const user = await requireUser();
-  
   //compare form data with zod schema
   const submission = parseWithZod(formData, {
     schema: PostSchema,
