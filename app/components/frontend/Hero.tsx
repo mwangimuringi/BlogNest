1edf189af51bd380e56e1b8e05c0f8d75184f812
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-20 14:04:30 +0300

    ...

--- app/components/frontend/Hero.tsx
+++ app/components/frontend/Hero.tsx
@@ -54,10 +54,6 @@ export function Hero() {
             <div className="flex items-center justify-center gap-x-5 mt-5">
               <LoginLink>
                 <Button variant="secondary">Sign in</Button>\
-                <span className="text-sm text-muted-foreground">
-                  or
-                  <br />
-                </span>
               </LoginLink>
               <RegisterLink>
                 <Button>Try it free</Button>
