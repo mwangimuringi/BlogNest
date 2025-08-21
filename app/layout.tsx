d2ebfdf534898affa28f8a1d2372c09c06d2dc39
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-21 13:53:10 +0300

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster richColors closeButton />
+          <style jsx global></style>
         </ThemeProvider>
       </body>
     </html>
