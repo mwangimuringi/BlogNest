d683259e6637ec3d65bf261b4947799cad6a7db6
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-21 13:00:09 +0300

    ...

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -78,9 +78,6 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
           </div>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="mb-4 flex items-center justify-between">
-            Current Site:{" "}
-          </div>
           {children}
         </main>
       </div>
