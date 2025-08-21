51e6434e3f914d12e6f3026f2d2f54b54c05f1a8
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-21 13:02:09 +0300

    ...

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -83,7 +83,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 
         <footer className="flex h-14 items-center border-t bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <div className="ml-auto flex items-center gap-x-5">
-            <ThemeToggle />
+            <ThemeToggle = [] />
           </div>
         </footer>
       </div>
