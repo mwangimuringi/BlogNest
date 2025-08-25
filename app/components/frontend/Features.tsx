cd3d17de5f5a32c5f20015eea699ed78b5ea96fb
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-25 15:08:58 +0300

    ...

--- app/components/frontend/Features.tsx
+++ app/components/frontend/Features.tsx
@@ -56,7 +56,7 @@ export function Features() {
               <p className="mt-2 text-sm text-muted-foreground leading-snug">
                 {feature.description}
                 <span className="block mt-2 text-primary">
-                  
+                  <h1>Get started</h1>
                 </span>
               </p>
             </div>
