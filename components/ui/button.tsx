3ce40977359c0e4db0a2e71cecbf8ece4e983dd3
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-26 16:13:06 +0300

    ...

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -36,8 +36,6 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-      asChild?: boolean
-      icon?: React.ReactNode
   asChild?: boolean
 }
 
