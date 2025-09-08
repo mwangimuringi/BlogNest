f748865ca4b5cb6f431ec699f1618fb500ca3016
mwangimuringi <mwangimuringi10@gmail.com>
2025-09-08 12:25:34 +0300

    ...

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -41,6 +41,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Run = asChild ? "span" : "button"
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
