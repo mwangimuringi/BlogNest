5faf97b7ef59045de6aa925bd622896b8d9d223c
mwangimuringi <mwangimuringi10@gmail.com>
2025-09-09 12:51:36 +0300

    ...

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -79,7 +79,9 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
-          <div className="mt-auto flex items-center justify-end gap-4"></div>
+          <div className="mt-auto flex items-center justify-end gap-4">
+            <Link href="/dashboard/settings"></Link>
+          </div>
         </main>
       </div>
     </section>
